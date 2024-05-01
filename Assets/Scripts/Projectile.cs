dbff215c00962449aaa53962274f8961c2a5ae6f
CodeNinjasSJ <codeninjassouthjordan@gmail.com>
2024-04-30 18:59:36 -0600

    fix code that adds +1 when touched shuriken
    page 56

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour
 {
     public Throwable direction;
     public float speed;
+    public Text counterText;
+    private int count = 0;
+    public GameObject player;
 
     private Vector3 dir;
     // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            count++;
+
+            if (counterText != null)
+            {
+                counterText.text = ": " + count.ToString();
+            }
+
+            Destroy(gameObject);
+        }
+    }
+    
     void Start()
     {
         dir  = direction.offset;
